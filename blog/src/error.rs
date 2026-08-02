7e0e615bcb40286f7da790ed601fe2ea262f4292
Draken1974 <gudhalarya@gmail.com>
2026-08-02 21:25:52 +0530

    This is the custom errors

---
+++ blog/src/error.rs
@@ -0,0 +1,39 @@
+use actix_web::{HttpResponse, ResponseError, http::StatusCode};
+//This is the errors file that will hold the custom errors we will write 
+use thiserror::Error;
+
+#[derive(Debug,Error)]
+pub enum AppError {
+    #[error( "Internal Server error occurred ")]
+    InternalServerError(#[from]anyhow::Error),
+
+    #[error("Not Found")]
+    NotFound,
+
+    #[error("Unauthorized")]
+    Unauthorized
+}
+
+use serde::Serialize;
+#[derive(Debug,Serialize)]
+pub struct ErrorResponse{
+    error:String
+}
+
+impl ResponseError for AppError{
+    fn status_code(&self) -> StatusCode {
+        match self{
+            AppError::InternalServerError(_)=>StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::Unauthorized=>StatusCode::UNAUTHORIZED,
+            AppError::NotFound=>StatusCode::NOT_FOUND
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        if let AppError::InternalServerError(err) =self{
+            tracing::error!("Internal error : {:?}",err);
+        } 
+        HttpResponse::build(self.status_code()).json(ErrorResponse{error:self.to_string(),
+        })
+    }
+}
--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,7 +1,12 @@
 /*This is the main file of the code*/
 //The first thing we will do is the connection to the database 
 mod db;
-use actix_web::{App, HttpResponse, HttpServer, post};
+mod error;
+use actix_web::{App, HttpResponse, HttpServer, post, web};
+
+use crate::db::get_db;
+
+//This is to check the health of the server 
 #[post("/health")]
 async fn health()->HttpResponse{
     HttpResponse::Ok().json(serde_json::json!({"Ok":"Status is ok "}))
@@ -10,8 +15,12 @@ async fn health()->HttpResponse{
 //This is the main server 
 #[actix_web::main]
 async fn main ()->std::io::Result<()>{
+    tracing_subscriber::fmt().init();
+    let pool = get_db().await;
     HttpServer::new(move||{
         App::new()
+        .wrap(actix_web::middleware::Logger::default())
+        .app_data(web::Data::new(pool.clone()))
         .service(health)
     })
     .bind(("127.0.0.1",8080))?
